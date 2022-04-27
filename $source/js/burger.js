9a42a50dd2236b0e32ddb821de888796a4a5e6cf
NazPalUA <UA.Nazar.Palamarchuk.UA@gmail.com>
2022-04-27 15:40:33 +0300

    finished adaptivity

--- $source/js/burger.js
+++ $source/js/burger.js
@@ -4,4 +4,5 @@ const navList = document.querySelector('.nav__list');
 burger.addEventListener('click', ()=>{
   navList.classList.toggle('active');
   burger.classList.toggle('active');
+  document.body.classList.toggle('lock');
 })
